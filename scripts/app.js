479147b62855c4dd7975af7b701baa423318090c
Andrew Watson <andrewpeterwatson@gmail.com>
2016-02-18 17:23:04 -0800

    created hero and fire

--- scripts/app.js
+++ scripts/app.js
@@ -1,32 +1,51 @@
+var CANVAS_WIDTH = 480;
+var CANVAS_HEIGHT = 320;
 
+var canvasElement = $("<canvas width='" + CANVAS_WIDTH +
+                      "' height='" + CANVAS_HEIGHT + "'></canvas>");
+var canvas = canvasElement.get(0).getContext("2d");
+canvasElement.appendTo('body');
 
-var canvas = document.getElementById("myCanvas");
-var ctx = canvas.getContext("2d");
-var x = canvas.width/2;
-var y = canvas.height-30;
-var dx = 2;
-var dy = -2;
-var playerHeight = 10;
-var playerWidth = 10;
-var playerX = (canvas.width - playerWidth) / 2;
-var bulletHeight = 10;
-var bulletWidth = 10;
-var bulletX = (canvas.width - bulletWidth) / 2;
+var FPS = 30;
+setInterval(function() {
+  update();
+  draw();
+}, 1000/FPS);
+
+function update() {}
+function draw() {}
+
+var textX = 50;
+var textY = 50;
+
+function update() {
+  textX += 1;
+  textY += 1;
+}
+var player = {
+  color: "black",
+  x: 220,
+  y: 270,
+  width: 10,
+  height: 10,
+  draw: function() {
+    canvas.fillStyle = this.color;
+    canvas.fillRect(this.x, this.y, this.width, this.height);
+  }
+};
+
+function draw() {
+  canvas.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  player.draw();
+  playerBullets.forEach(function(bullet) {
+  bullet.draw();
+});
+}
 var rightPressed = false;
 var leftPressed = false;
 var spacePressed = false;
-
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
-
-function drawBullet() {
-    ctx.beginPath();
-    ctx.rect(playerX, y, bulletWidth, bulletHeight);
-    ctx.fillStyle = "red";
-    ctx.fill();
-    ctx.closePath();
-}
-
 
 function keyDownHandler(e) {
     if(e.keyCode === 39) {
@@ -48,31 +67,64 @@ function keyUpHandler(e) {
 
 }
 
-
-function drawPlayer() {
-    ctx.beginPath();
-    ctx.rect(playerX, canvas.height - playerHeight, playerWidth, playerHeight);
-    ctx.fillStyle = "black";
-    ctx.fill();
-    ctx.closePath();
-}
-
-function draw() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawPlayer();
-    drawBullet();
-
-
-    if(rightPressed && playerX < canvas.width - playerWidth) {
-        playerX += 7;
-    } else if(leftPressed && playerX > 0) {
-        playerX -= 7;
+function update() {
+  if (spacePressed) {
+  player.shoot();
+  } else if (leftPressed) {
+      player.x -= 5;
+    } else if (rightPressed) {
+      player.x += 5;
     }
-    
 
-    x += dx;
-    y += dy;
+    playerBullets.forEach(function(bullet) {
+    bullet.update();
+  });
 }
+var playerBullets = [];
 
+function Bullet(I) {
+  I.active = true;
 
-setInterval(draw, 10);
+  I.xVelocity = 0;
+  I.yVelocity = -I.speed;
+  I.width = 3;
+  I.height = 3;
+  I.color = "black";
+
+  I.inBounds = function() {
+    return I.x >= 0 && I.x <= CANVAS_WIDTH &&
+      I.y >= 0 && I.y <= CANVAS_HEIGHT;
+  };
+
+  I.draw = function() {
+    canvas.fillStyle = this.color;
+    canvas.fillRect(this.x, this.y, this.width, this.height);
+  };
+
+  I.update = function() {
+    I.x += I.xVelocity;
+    I.y += I.yVelocity;
+
+    I.active = I.active && I.inBounds();
+  };
+
+  return I;
+}
+player.shoot = function() {
+  var bulletPosition = this.midpoint();
+  playerBullets.push(Bullet({
+    speed: 5,
+    x: bulletPosition.x,
+    y: bulletPosition.y
+  }));
+};
+
+player.midpoint = function() {
+  return {
+    x: this.x + this.width/2,
+    y: this.y + this.height/2
+  };
+};
+  playerBullets = playerBullets.filter(function(bullet) {
+    return bullet.active;
+  });
--- scripts/movementTest.js
+++ scripts/movementTest.js
@@ -1,107 +1,139 @@
-var canvas = document.getElementById('canvas');
+var CANVAS_WIDTH = 480;
+var CANVAS_HEIGHT = 320;
 
-if (canvas.getContext) {
-    // Grab our context
-    var context = canvas.getContext('2d');
+var canvasElement = $("<canvas width='" + CANVAS_WIDTH +
+                      "' height='" + CANVAS_HEIGHT + "'></canvas>");
+var canvas = canvasElement.get(0).getContext("2d");
+canvasElement.appendTo('body');
 
-    // Make sure we have a valid defintion of requestAnimationFrame
-    var requestAnimationFrame =
-            window.requestAnimationFrame ||
-            window.webkitRequestAnimationFrame ||
-            window.mozRequestAnimationFrame ||
-            window.msRequestAnimationFrame ||
-            window.oRequestAnimationFrame ||
-            function(callback) {
-                return setTimeout(callback, 16);
-            };
+var FPS = 30;
+setInterval(function() {
+  update();
+  draw();
+}, 1000/FPS);
 
-    // Let's define our square
-    var square = {
-        'x': 450,
-        'y': 600,
-        'width': 15,
-        'height': 15,
-        'fill': '#000000'
-    };
+function update() {}
+function draw() {}
 
-    var render = function() {
-        // Clear the canvas
-        context.clearRect(0, 0, canvas.width, canvas.height);
+var textX = 50;
+var textY = 50;
 
-        // Draw the square
-        context.beginPath();
-        context.rect(square.x, square.y, square.width, square.height);
-        context.fillStyle = square.fill;
-        context.fill();
-
-        // Redraw
-        requestAnimationFrame(render);
-    };
-
-    // Start the redrawing process
-    render();
-
-    var animate = function(prop, val, duration) {
-      // The calculations required for the step function
-      var start = new Date().getTime();
-      var end = start + duration;
-      var current = square[prop];
-      var distance = val - current;
-
-      var step = function() {
-        // Get our current progres
-        var timestamp = new Date().getTime();
-        var progress = Math.min((duration - (end - timestamp)) / duration, 1);
-
-        // Update the square's property
-        square[prop] = current + (distance * progress);
-
-        // If the animation hasn't finished, repeat the step.
-        if (progress < 1) requestAnimationFrame(step);
-      };
-
-      // Start the animation
-      return step();
-    };
-
-    var meta = function(e) {
-        // Set some initial variables
-        var distance = 100;
-        var prop = 'x';
-        var mult = 1;
-
-        // Just return false if the key isn't an arrow key
-        if (e.which < 37 || e.which > 40) {
-            return false;
-        };
-
-        // If we're going left or up, we want to set the multiplier to -1
-        if (e.which === 37 || e.which === 38) {
-            mult = -1;
-        }
-
-        // If we're going up or down, we want to change the property we will be animating.
-        if (e.which === 38 || e.which === 40) {
-            prop = 'y';
-        };
-
-        return [prop, mult * distance];
-    };
-
-    document.body.addEventListener('keydown', function(e) {
-        var info = meta(e);
-        if (info) {
-            e.preventDefault();
-            animate(info[0], square[info[0]] + info[1], 1000);
-        };
-    });
-
-    document.body.addEventListener('keyup', function(e) {
-        var info = meta(e);
-
-        if (info) {
-            e.preventDefault();
-            animate(info[0], square[info[0]], 1000);
-        };
-    });
+function update() {
+  textX += 1;
+  textY += 1;
+}
+var player = {
+  color: "black",
+  x: 220,
+  y: 270,
+  width: 10,
+  height: 10,
+  draw: function() {
+    canvas.fillStyle = this.color;
+    canvas.fillRect(this.x, this.y, this.width, this.height);
+  }
 };
+
+function draw() {
+  canvas.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  player.draw();
+  playerBullets.forEach(function(bullet) {
+  bullet.draw();
+});
+}
+var rightPressed = false;
+var leftPressed = false;
+var spacePressed = false;
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
+function keyDownHandler(e) {
+    if(e.keyCode === 39) {
+        rightPressed = true;
+    } else if(e.keyCode === 37) {
+        leftPressed = true;
+    } else if(e.keyCode === 32) {
+      spacePressed = true;
+    }
+}
+function keyUpHandler(e) {
+    if(e.keyCode === 39) {
+        rightPressed = false;
+    } else if(e.keyCode === 37) {
+        leftPressed = false;
+    } else if(e.keyCode === 32) {
+      spacePressed = false;
+    }
+
+}
+
+function update() {
+  if (spacePressed) {
+  player.shoot();
+}
+  if (leftPressed) {
+    player.x -= 5;
+  }
+
+  if (rightPressed) {
+    player.x += 5;
+  }
+
+  playerBullets.forEach(function(bullet) {
+  bullet.update();
+});
+
+playerBullets = playerBullets.filter(function(bullet) {
+  return bullet.active;
+});
+}
+var playerBullets = [];
+
+function Bullet(I) {
+  I.active = true;
+
+  I.xVelocity = 0;
+  I.yVelocity = -I.speed;
+  I.width = 3;
+  I.height = 3;
+  I.color = "black";
+
+  I.inBounds = function() {
+    return I.x >= 0 && I.x <= CANVAS_WIDTH &&
+      I.y >= 0 && I.y <= CANVAS_HEIGHT;
+  };
+
+  I.draw = function() {
+    canvas.fillStyle = this.color;
+    canvas.fillRect(this.x, this.y, this.width, this.height);
+  };
+
+  I.update = function() {
+    I.x += I.xVelocity;
+    I.y += I.yVelocity;
+
+    I.active = I.active && I.inBounds();
+  };
+
+  return I;
+}
+
+player.shoot = function() {
+  var bulletPosition = this.midpoint();
+
+  playerBullets.push(Bullet({
+    speed: 5,
+    x: bulletPosition.x,
+    y: bulletPosition.y
+  }));
+};
+
+player.midpoint = function() {
+  return {
+    x: this.x + this.width/2,
+    y: this.y + this.height/2
+  };
+};
+  playerBullets = playerBullets.filter(function(bullet) {
+    return bullet.active;
+  });

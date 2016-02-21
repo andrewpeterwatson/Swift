0e9b0d610699befad645f9a80d5bd6eedd48a9ed
Andrew Watson <andrewpeterwatson@gmail.com>
2016-02-21 15:11:07 -0800

    created collision detection

--- scripts/player.js
+++ scripts/player.js
@@ -7,6 +7,8 @@ var canvas = ctx.getContext("2d");
 
 var imgBg;
 var imgDrops;
+var ballHeight = 80;
+var ballWidth = 80;
 var ballX = 0;
 var ballY = 0;
 var fallingDrops = [];
@@ -20,15 +22,13 @@ function drawBallz() {
   ctx4.clearRect(0, 0, canvas4.width, canvas4.height);
 
 
-    for (var i=0; i< noOfDrops; i++)
-    {
-    ctx4.drawImage (fallingDrops[i].image, fallingDrops[i].ballX, fallingDrops[i].ballY, 80, 80); //The rain drop
-
-    fallingDrops[i].ballY += fallingDrops[i].speed; //Set the falling speed
-    if (fallingDrops[i].ballY > 750) {  //Repeat the raindrop when it falls out of view
-    fallingDrops[i].ballY = -25 //Account for the image size
-    fallingDrops[i].ballX = Math.random() * 1300;    //Make it appear randomly along the width
-    }
+    for (var i=0; i< noOfDrops; i++) {
+      ctx4.drawImage (fallingDrops[i].image, fallingDrops[i].ballX, fallingDrops[i].ballY, ballHeight, ballWidth); //The rain drop
+      fallingDrops[i].ballY += fallingDrops[i].speed; //Set the falling speed
+      if (fallingDrops[i].ballY > 750) {  //Repeat the raindrop when it falls out of view
+        fallingDrops[i].ballY = - 25 //Account for the image size
+        fallingDrops[i].ballX = Math.random() * 1300;    //Make it appear randomly along the width
+      }
 
     }
 }
@@ -39,23 +39,27 @@ function setup() {
           ctx4 = canvas4.getContext('2d');
 
   setInterval(drawBallz, 36);
+
+  function FireBall() {
+    this.height = ballHeight;
+    this.width = ballWidth;
+    this.speed = Math.random() * 9;
+    this.image = new Image();
+    this.image.src = 'http://vignette4.wikia.nocookie.net/mario/images/f/fa/494px-Fireball_Artwork_-_Super_Mario_3D_World.png/revision/latest?cb=20131129222802';
+  }
+
   for (var i = 0; i < noOfDrops; i++) {
-      var fallingDr = new Object();
-      fallingDr["image"] =  new Image();
-  fallingDr.image.src = 'http://vignette4.wikia.nocookie.net/mario/images/f/fa/494px-Fireball_Artwork_-_Super_Mario_3D_World.png/revision/latest?cb=20131129222802';
+      var fallingDr = new FireBall();
+      // fallingDr["image"] =  new Image();
 
       fallingDr["ballX"] = Math.random() * 1300;
-      fallingDr["ballY"] = -1000;
-      fallingDr["speed"] = Math.random() * 9;
+      fallingDr["ballY"] = - 1000;
+      // fallingDr["speed"] = Math.random() * 9;
       fallingDrops.push(fallingDr);
       }
 
   }
 }
-
-
-
-
 setup();
 
 
@@ -128,7 +132,7 @@ function update() {
     } else if (rightPressed) {
       player.x += 5;
     }
-
+    checkCollisions();
     playerBullets.forEach(function(bullet) {
     bullet.update();
   });
@@ -137,7 +141,6 @@ var playerBullets = [];
 
 function Bullet(I) {
   I.active = true;
-
   I.xVelocity = 0;
   I.yVelocity = -I.speed;
   I.width = 3;
@@ -157,7 +160,6 @@ function Bullet(I) {
   I.update = function() {
     I.x += I.xVelocity;
     I.y += I.yVelocity;
-
     I.active = I.active && I.inBounds();
   };
 
@@ -178,6 +180,32 @@ player.midpoint = function() {
     y: this.y + this.height/2
   };
 };
+
+
+
   playerBullets = playerBullets.filter(function(bullet) {
     return bullet.active;
   });
+
+  // function collisionDetection(object1,obeject2) {
+  //   return (object1.x < object2.x + object2.width  && object1.x + object1.width  > object2.x &&
+	// 	object1.y < object2.y + object2.height && object1.y + object1.height > object2.y)
+  // }
+  function checkCollisions() {
+    // for (var i = 0; i < playerBullets.length; i++) {
+    //     for (var i = 0; i < fallingDrops.length; i++) {
+    //       if (player.x < fallingDrops[i].ballX + fallingDrops[i].width  && player.x + 3  > fallingDrops[i].ballX &&
+    //      		playerBullets[i].y < fallingDrops[i].ballY + fallingDrops[i].height && playerBullets[i].y + playerBullets[i].height > fallingDrops[i].ballY){
+    //           console.log("lava dead");
+    //         // Bullet.active = false;
+    //     };
+    //   };
+    // };
+    for (var i = 0; i < fallingDrops.length; i++) {
+      if (fallingDrops[i].ballX < player.x + player.width  && fallingDrops[i].ballX + fallingDrops[i].width  > player.x &&
+    		fallingDrops[i].ballY < player.y + player.height && fallingDrops[i].ballY + fallingDrops[i].height > player.y) {
+        console.log("player died");
+        // player.explode();
+      }
+    }
+  }

aadb0a047e0410cfcd5f793f27563d7c94447bed
Daniel Wade <dcwade87@gmail.com>
2016-02-22 21:19:41 -0800

    script for dom manipulation

---
+++ scripts/instruction.js
@@ -0,0 +1,21 @@
+document.onkeydown = checkKey;
+
+function checkKey(e) {
+
+    e = e || window.event;
+
+    if (e.keyCode == '32') {
+      document.getElementById("fire").src="media/testImg/toonbomb.jpg";
+
+    } else if (e.keyCode == '37') {
+      document.getElementById("arrows").src="media/testImg/left.jpg";
+
+    } else if (e.keyCode == '39') {
+      document.getElementById("arrows").src="media/testImg/right.jpg";
+    };
+};
+
+function restoreImage() {
+  document.getElementById("arrows").src="media/testImg/up.jpg";
+  document.getElementById("fire").src="media/testImg/bomb.jpg";
+};
